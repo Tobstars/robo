24cb8c325fd1883283b5a85bdd6624d83400028b
Tobias <tobi.deesbach@t-online.de>
2020-03-28 15:06:50 +0100

    Remove secondary controller

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private void doFoo() throws IOException {
     }
 }
