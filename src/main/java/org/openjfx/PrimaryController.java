63260cf04d3be890c9f44c5b2dd86506ff0b023f
Tobias <tobi.deesbach@t-online.de>
2020-03-28 21:27:36 +0100

    Add dragging and placement logic

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,11 +1,85 @@
 package org.openjfx;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PrimaryController {
 
     @FXML
-    private void doFoo() throws IOException {
+    ImageView robotStart;
+    @FXML
+    ImageView robotEnd;
+    @FXML
+    Label info;
+    boolean placementFinished;
+    boolean dragging;
+
+    @FXML
+    private void dragRobotStart(MouseEvent e) {
+        dragging = true;
+        robotStart.setX(e.getX());
+        robotStart.setY(e.getY());
+    }
+
+    @FXML
+    private void dragRobotEnd(MouseEvent e) {
+        dragging = true;
+        robotEnd.setX(e.getX());
+        robotEnd.setY(e.getY());
+    }
+
+    @FXML
+    private void showRobotEnd() {
+        // only show the robot end if the placement is not finished
+        if (!placementFinished) {
+            robotEnd.setVisible(true);
+            info.setText("Place the robot on the end position");
+        }
+        dragging = false;
+    }
+
+    @FXML
+    private void showRobotStartName() {
+        // Only show text if robot was placed before and if is is not dragged at the moment
+        if (placementFinished && !dragging) {
+            info.setText("Currently selected: Start Placement");
+        }
+    }
+
+    @FXML
+    private void showRobotEndName() {
+        // Only show text if robot was placed before and if it is not dragged at the moment
+        if (placementFinished && !dragging) {
+            info.setText("Currently selected: End Placement");
+        }
+    }
+
+    @FXML
+    private void setPlacingFinished() {
+        placementFinished = true;
+        dragging = false;
+        clearInfo();
+    }
+
+    @FXML
+    private void clearInfo() {
+        if (placementFinished) {
+            info.setText("");
+        }
+    }
+
+    @FXML
+    private void reset() {
+        info.setText("Place the robot on the start position");
+        placementFinished = false;
+
+        robotStart.setX(0);
+        robotStart.setY(0);
+
+        robotEnd.setVisible(false);
+        robotEnd.setX(0);
+        robotEnd.setY(0);
     }
 }
